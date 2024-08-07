e1c5eb184200d6972c1a6f7a58f9d4ba04a11850
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2024-08-08 01:01:57 +0530

    Create variables.tf

---
+++ modules/autoscaling_group/variables.tf
@@ -0,0 +1,24 @@
+variable "subnet_id" {
+  description = "The ID of the subnet to launch instances in"
+  type        = string
+}
+
+variable "launch_configuration_id" {
+  description = "The ID of the Launch Configuration"
+  type        = string
+}
+
+variable "asg_min_size" {
+  description = "The minimum size of the Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_max_size" {
+  description = "The maximum size of the Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_desired_capacity" {
+  description = "The desired capacity of the Auto Scaling Group"
+  type        = number
+}
