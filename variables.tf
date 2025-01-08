9f4b26226c6e1e1dd9a727c69d61cc71c5ec2f3e
Saravanesh <126149128+gitsaravanesh@users.noreply.github.com>
2025-01-08 19:18:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "instance_type" {
